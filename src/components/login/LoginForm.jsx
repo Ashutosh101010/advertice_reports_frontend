59e1730ae7273ed8f3d0fd6690517edee6c6b31e
Rohit Tatwade <tatwaderohit369@gmail.com>
2025-06-17 16:24:30 +0530

    Login Logo CHange

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Button, Grid, IconButton, InputAdornment, TextField, Typography, useMediaQuery } from "@mui/material";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import loginLogo from "../../assets/logo.png"
+import loginLogo from "../../assets/advertice-logo.png"
 import AdverticeNetwork from "../../Network";
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import VisibilityIcon from '@mui/icons-material/Visibility';
